6ee7ffd1056c23e30c7bcdbf43a4740558505d70
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-04-03 22:47:59 -0600

    Routing application and making basic components structure, pages and design.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticlesList from './pages/ArticlesList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+    {/* Routing components with React Router */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticlesList />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div><div className="mb-20">
+    <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">About</h1>
+    <p className="mx-auto leading-relaxed text-based mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis blandit felis, eget tincidunt urna porta sed. In a erat viverra, luctus orci sit amet, porta tellus. Aenean vehicula sodales neque nec scelerisque. Aenean nec ipsum quis risus dignissim sollicitudin et ac purus. Vestibulum cursus purus non magna dictum semper. Duis elit odio, aliquam in est vel, tristique elementum felis. Donec orci libero, scelerisque quis elit at, bibendum laoreet lacus. Nulla maximus massa vitae lorem viverra, commodo porttitor purus sollicitudin. Pellentesque sed lacus ullamcorper, aliquet urna consequat, lobortis magna. Mauris sit amet accumsan metus. Vivamus ac consequat turpis. Vestibulum placerat augue et placerat cursus. Integer euismod massa rhoncus ullamcorper commodo.</p>
+
+    <p className="mx-auto leading-relaxed text-based mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis blandit felis, eget tincidunt urna porta sed. In a erat viverra, luctus orci sit amet, porta tellus. Aenean vehicula sodales neque nec scelerisque. Aenean nec ipsum quis risus dignissim sollicitudin et ac purus. Vestibulum cursus purus non magna dictum semper. Duis elit odio, aliquam in est vel, tristique elementum felis. Donec orci libero, scelerisque quis elit at, bibendum laoreet lacus. Nulla maximus massa vitae lorem viverra, commodo porttitor purus sollicitudin. Pellentesque sed lacus ullamcorper, aliquet urna consequat, lobortis magna. Mauris sit amet accumsan metus. Vivamus ac consequat turpis. Vestibulum placerat augue et placerat cursus. Integer euismod massa rhoncus ullamcorper commodo.</p>
+
+    <p className="mx-auto leading-relaxed text-based mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis blandit felis, eget tincidunt urna porta sed. In a erat viverra, luctus orci sit amet, porta tellus. Aenean vehicula sodales neque nec scelerisque. Aenean nec ipsum quis risus dignissim sollicitudin et ac purus. Vestibulum cursus purus non magna dictum semper. Duis elit odio, aliquam in est vel, tristique elementum felis. Donec orci libero, scelerisque quis elit at, bibendum laoreet lacus. Nulla maximus massa vitae lorem viverra, commodo porttitor purus sollicitudin. Pellentesque sed lacus ullamcorper, aliquet urna consequat, lobortis magna. Mauris sit amet accumsan metus. Vivamus ac consequat turpis. Vestibulum placerat augue et placerat cursus. Integer euismod massa rhoncus ullamcorper commodo.</p>
+</div></div>
+  );
+};
+
+export default About;
